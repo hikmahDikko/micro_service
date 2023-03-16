0d99eb6cb6a584fa5a1a90b9ee99ec01c9990b26
hikmahDikko <dhywis31@gmail.com>
2023-03-16 11:44:08 +0100

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import PostCreate from "./post_create"
+import PostCreate from "./post_create";
+import PostList from "./post_list"
 
 export default () => {
     return <div className="container">
         <h1>Create Post</h1>
         <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
     </div>;
 };
