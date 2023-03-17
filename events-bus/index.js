bf09199fea7f494fa458e92e97b54d2d54f3f029
hikmahDikko <dhywis31@gmail.com>
2023-03-17 22:39:28 +0100

    initial commit

--- events-bus/index.js
+++ events-bus/index.js
@@ -6,8 +6,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', async (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     await axios.post("http://localhost:4000/events", event);
     await axios.post("http://localhost:4001/events", event);
@@ -17,6 +21,10 @@ app.post('/events', async (req, res) => {
     res.send({ status : 'OK' });
 });
 
+app.get("/events", (req, res) => {
+    res.send(events);
+});
+
 app.listen(4005, () => {
-    console.log("listening on 4005");
+    console.log("Listening on 4005");
 });
